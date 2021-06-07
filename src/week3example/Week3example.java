96d11ec0e9bde039eefabb19688e7154d7daaf74
garnetmi <garnetmi@sheridancollege.ca>
2021-06-07 14:18:52 -0400

    week 3 Local rep

---
+++ src/week3example/Week3example.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week3example;
+
+/**
+ *
+ * @author lilmi
+ */
+public class Week3example {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hello World today is week 3");
+        // today is Monday and week 3
+    }
+    
+}
